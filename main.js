7c6f6c57303fc584bc4805896423c457a651f247
Peterson Winny Babu <88373271+PetersonWinnyBabu@users.noreply.github.com>
2023-03-27 13:16:15 +0530

    changed

--- main.js
+++ main.js
@@ -9,7 +9,19 @@ showTextNode(1)
 let popup = document.getElementById("popup");
 let popup1= document.getElementById("popup1");
 
+
 }
+
+
+function openPopup(){
+  popup.classList.add("open-popup");
+}
+
+function closePopup(){
+  popup.classList.remove("open-popup");
+
+}
+
 
 function showTextNode(textNodeIndex) {
 
@@ -47,72 +59,61 @@ function selectOption(option) {
     showTextNode(nextTextNodeId)
   }
   
-function openPopup(){
-  popup.classList.add("open-popup");
-}
-
-function closePopup(){
-  popup.classList.remove("open-popup");
-
-}
-function openPopup1(){
-  popup1.classList.add("open-popup1");
-}
-
-function closePopup1(){
-  popup1.classList.remove("open-popup1");
-}
 
 
 const textNodes = [
     {
       id: 1,
-      text: 'You hear your alarm going off, you overslept! It is 30 mins to your first Lecture.',
+      text: "You wake up feeling anxious and overwhelmed. What do you do?",
       options: [
         {
-          text: 'Wake Up!' ,
+          text: "Stay in bed and avoid the world" ,
           //setState: { sleep: true },
           nextText:2
+          
 
         },
         {
-          text: 'Hit Snooze',
-          nextText: 2
+          text: "Get up and face the day",
+          nextText:3
         }
       ]
     },
     {
       id: 2,
-      text: 'Looks like your efforts were useless. You look at the clock again and you have missed your first lecture',
+      text: "You spend the day in bed, feeling worse and more alone. You realize you need help. What do you do?",
       options: [
         {
-          text: 'Wake up for your second class',
+          text: "Ignore your feelings and hope they go away",
           //requiredState: (currentState) => currentState.sleep,
           setState: { sleep: false, wake: true },
           nextText: 4
         },
         {
-          text: 'Just go back to sleep',
+          text: "Seek help from a therapist",
           //requiredState: (currentState) => currentState.sleep,
           setState: { sleep: true, wake: false },
           nextText: 5
         },
-        {
-          text: 'Smash the alarm clock against the wall',
-          setState: { sleep: true, wake: false },
-          nextText: 3
-        }
+
+        
       ]
     },
 
     {
         id: 3,
-        text: 'You keep sleeping thorughout the day, and you have missed all your classes. You are unable to get out of bed :(',
+        text: "You face the day and try to stay positive, but you still feel overwhelmed. What do you do?",
         options: [
             {
-              text: 'Restart',
-              nextText: -1
+              text: "Try a mindfulness exercise",
+              nextText:6
             },
+            
+            {
+              text: "Find a supportive friend to talk to",
+              nextText:7
+
+            }
 
           ]
         
@@ -120,15 +121,15 @@ const textNodes = [
       },
       {
         id: 4,
-        text: 'You rush out, skipping breakfast (like always), and you catch the metro. You end up dosing off on the metro, missing your station. You are woken up by the train conduct',
+        text: "Ignoring your feelings only makes them worse. You start to spiral and realize you need help. What do you do?",
         options: [
             {
-              text: 'Take the train back to attend your last lecture',
-              nextText: 6
+              text: "Keep ignoring your feelings and hope they go away",
+              nextText:4
             },
             {
-              text: 'Go back home, because what is the point anyway now?',
-              nextText: 7
+              text:"Seek help from a therapist",
+              nextText:5
             },
            
           ]
@@ -137,11 +138,11 @@ const textNodes = [
 
       {
         id: 5,
-        text: 'You are woken up by your guardian, telling you that you have skipped enough classes and that you will have to repeat the semester. ',
+        text: "You take the first step towards healing by seeking help from a therapist. They provide you with resources and support to help you manage your mental health.",
         options: [
             {
-                text: 'Restart',
-                nextText: -1
+                text: "Continue on your journey",
+                nextText:8
             },
            
            
@@ -152,11 +153,17 @@ const textNodes = [
       
       {
         id: 6,
-        text: 'You barely make it to class. You feel so drained and your vision become blurry... You pass out due to exhaustion ',
+        text: "You try a mindfulness exercise to reduce stress and find that it helps. What do you do next?",
         options: [
             {
-                text: 'Restart',
-                nextText: -1
+                text: "Find more resources for managing stress",
+                nextText:8
+            },
+            
+            {
+              text: "Take a break and rest",
+              nextText:7
+
             },
            
            
@@ -165,18 +172,35 @@ const textNodes = [
 
       {
         id: 7,
-        text: 'GAME OVER',
+        text: "You find a supportive friend to talk to and feel less alone. What do you do next?",
         options: [
             {
-                text: 'START AGAIN',
-                nextText: -1
+                text: "Try to build more meaningful connections with others",
+                nextText:8
             },
+            {
+              text: "Take a break and focus on self-care",
+              nextText:7
+            }
+
+          
            
            
           ]
       
 
-      }
+      },
+
+      {
+        id: 8,
+        text: "Congratulations! You have completed the game and helped the character improve their mental health. Remember to prioritize your own mental health and seek help if needed.",
+        options: [
+            {
+                text:"RESTART",
+                nextText:1
+            },
+          ]
+      },
 
       
 
